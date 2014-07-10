82c4496aeda7904039d4bf8a77b2a7efa329a34b
Ujjwal Ojha <bring2uo@gmail.com>
2014-07-10 21:19:17 +0545

    Created watermark filter

---
+++ lib/Imagine/Filter/Basic/Watermark.php
@@ -0,0 +1,128 @@
+<?php
+
+/*
+ * This file is part of the Imagine package.
+ *
+ * (c) Bulat Shakirzyanov <mallluhuct@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Imagine\Filter\Basic;
+
+use Imagine\Image\ImageInterface;
+use Imagine\Image\Box;
+use Imagine\Image\Point;
+use Imagine\Filter\FilterInterface;
+use Imagine\Exception;
+
+class Watermark implements FilterInterface
+{
+    /**
+     * @var int|null
+     */
+    protected $size;
+
+    /**
+     * @var string
+     */
+    protected $position;
+
+    /**
+     * @var ImageInterface
+     */
+    protected $watermark;
+
+    /**
+     * Constructor
+     *
+     * @param ImageInterface  $watermark
+     * @param string|int|null $size
+     * @param string          $position
+     */
+    public function __construct(ImageInterface $watermark, $size = null, $position = 'center')
+    {
+        $this->watermark = $watermark;
+        $this->position = $position;
+        if (!is_null($size)) {
+            if (is_string($size) && substr($size, -1) === '%') {
+                $size = substr($size, 0, -1) / 100;
+            }
+            if (!is_numeric($size))) {
+                throw new Exception\InvalidArgumentException(
+                    sprintf('"%s" expects parameter 2, size to be a number, %s provider instead', __METHOD__, gettype($size))
+                );
+            }
+            $this->size = $size;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function apply(ImageInterface $image)
+    {
+        $watermark = $this->watermark;
+
+        $size = $image->getSize();
+        $watermarkSize = $watermark->getSize();
+
+        // If 'null': Downscale if needed
+        if (!$this->size && ($size->getWidth() < $watermarkSize->getWidth() || $size->getHeight() < $watermarkSize->getHeight())) {
+            $this->size = 1.0;
+        }
+
+        if ($this->size) {
+            $factor = $this->size * min($size->getWidth() / $watermarkSize->getWidth(), $size->getHeight() / $watermarkSize->getHeight());
+
+            $watermark->resize(new Box($watermarkSize->getWidth() * $factor, $watermarkSize->getHeight() * $factor));
+            $watermarkSize = $watermark->getSize();
+        }
+
+        switch ($this->position) {
+            case 'topleft':
+                $x = 0;
+                $y = 0;
+                break;
+            case 'top':
+                $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
+                $y = 0;
+                break;
+            case 'topright':
+                $x = $size->getWidth() - $watermarkSize->getWidth();
+                $y = 0;
+                break;
+            case 'left':
+                $x = 0;
+                $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
+                break;
+            case 'center':
+                $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
+                $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
+                break;
+            case 'right':
+                $x = $size->getWidth() - $watermarkSize->getWidth();
+                $y = ($size->getHeight() - $watermarkSize->getHeight()) / 2;
+                break;
+            case 'bottomleft':
+                $x = 0;
+                $y = $size->getHeight() - $watermarkSize->getHeight();
+                break;
+            case 'bottom':
+                $x = ($size->getWidth() - $watermarkSize->getWidth()) / 2;
+                $y = $size->getHeight() - $watermarkSize->getHeight();
+                break;
+            case 'bottomright':
+                $x = $size->getWidth() - $watermarkSize->getWidth();
+                $y = $size->getHeight() - $watermarkSize->getHeight();
+                break;
+            default:
+                throw new Exception\InvalidArgumentException(
+                    sprintf('Unknown position "%s"', $this->position)
+                );
+        }
+
+        return $image->paste($watermark, new Point($x, $y));
+    }
+}
